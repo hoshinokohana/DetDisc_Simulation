a3e9bdc3d1fd81f28e042ed6a4e3e8584e917f0b
Shiyi <stephensuns@protonmail.com>
2024-04-18 03:27:20 +0900

    shading update1.0

--- PE_bias/shading/shading.py
+++ PE_bias/shading/shading.py
@@ -1,87 +1,61 @@
 import torch
-
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
-import numpy as np
-
-
-
+# Function to load MNIST data
 def load_mnist_data():
-
-    """Load MNIST dataset with normalization."""
-
     transform = transforms.Compose([
-
-        transforms.ToTensor(),  # Converts image to tensor.
-
-        transforms.Normalize((0.5,), (0.5,))  # Normalizes pixel values.
-
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))  # Normalizes pixel values
     ])
-
     mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-
     return mnist_data
 
-
-
+# Function to extract images for a specific digit
 def get_digit_images(data, digit, n_images):
-
-    """Extracts images for a specific digit."""
-
     digit_images = [img for img, target in data if target == digit]
-
     return torch.stack(digit_images[:n_images])
 
-
-
-
+# Function to apply contrast adjustment to a batch of images
 def apply_contrast(images, contrast):
     """Applies contrast adjustment to a batch of images, ensuring contrast is between 0 and 1."""
-    # Ensure contrast values are within the bounds [0, 1]
     contrast = max(0, min(contrast, 1))
-    
-    images = images.float() / 255.0  # Convert to float and normalize to [0, 1]
-    mean_intensity = images.mean(dim=(1, 2, 3), keepdim=True)
+    images = images.float()  # Assume input is already normalized to [0, 1] or [-1, 1]
+    mean_intensity = images.mean(dim=(2, 3), keepdim=True)
     adjusted_images = (images - mean_intensity) * contrast + mean_intensity
-    adjusted_images = torch.clamp(adjusted_images, 0, 1)  # Ensure values stay within [0, 1]
-    return (adjusted_images * 255).byte()  # Convert back to uint8
+    adjusted_images = torch.clamp(adjusted_images, -1, 1)  # Clamping to [-1, 1] to stay within normalized range
+    return adjusted_images
 
-def blend_images(image_set1, image_set2, proportion1, proportion2):
+# Function to blend two sets of images by taking the maximum value for each pixel
+def blend_images_max(image_set1, image_set2):
+    return torch.max(image_set1, image_set2)
 
-    return image_set1 * proportion1 + image_set2 * proportion2
-
-
-
-def create_blended_dataset(data, digit1, digit2, contrast1, contrast2, proportion1, proportion2, n_images):
-
-    images1 = get_digit_images(data, digit1, n_images)
-
-    images2 = get_digit_images(data, digit2, n_images)
-
+def create_blended_digit_dataset(data, digit1, digit2, n_images, contrast1, contrast2):
+    """
+    Creates a new dataset by blending images of two specified digits with individual contrast adjustments.
+    Args:
+        data: The MNIST dataset loaded with load_mnist_data().
+        digit1: The first digit to blend, with a specific contrast adjustment.
+        digit2: The second digit to blend, with a different contrast adjustment.
+        n_images: Number of images to blend for each digit.
+        contrast1: Contrast adjustment for the first digit's images.
+        contrast2: Contrast adjustment for the second digit's images.
     
+    Returns:
+        A tensor dataset of blended images.
+    """
+    # Extract images for both digits
+    images_digit1 = get_digit_images(data, digit1, n_images)
+    images_digit2 = get_digit_images(data, digit2, n_images)
 
-    images1_contrasted = apply_contrast(images1, contrast1)
+    # Apply contrast adjustment to each set of images
+    adjusted_images_digit1 = apply_contrast(images_digit1, contrast1)
+    adjusted_images_digit2 = apply_contrast(images_digit2, contrast2)
 
-    images2_contrasted = apply_contrast(images2, contrast2)
-
-    
-
-    blended_images = blend_images(images1_contrasted, images2_contrasted, proportion1, proportion2)
-
-    
+    # Blend images
+    blended_images = blend_images_max(adjusted_images_digit1, adjusted_images_digit2)
 
     return blended_images
-
-
-
-# Usage
-
-mnist_data = load_mnist_data()
-
-blended_dataset = create_blended_dataset(mnist_data, digit1=1, digit2=0, contrast1=1.5, contrast2=0.5, proportion1=0.6, proportion2=0.4, n_images=100)
-
-
-
 
 
 
--- PE_bias/shading/train_and_eval.py
+++ PE_bias/shading/train_and_eval.py
@@ -1,34 +1,8 @@
 import torch
-from torch.utils.data import DataLoader, TensorDataset
-import numpy as np
-
-batch_size = 32
-digits = (0, 1)  # Pair of digits for blending
-contrast = (0.1, 0.9)  # Contrast levels for each digit
-proportion = (0.6, 0.4)  # Proportions for blending
-n_images = 10000
-
-from architecture import *
-from util import log
-from shading import create_blended_dataset
-
-# Use argparse or set manually
-batch_size = 32
-digits = (2, 7)
-
-contrast = (0.1, 0.9)
-
-proportion = (0.6, 0.4)
-n_images = 10000
-def generate_data_loader(digit1, digit2, contrast1, contrast2, n_images, batch_size):
-    blended_images = create_blended_dataset(digit1, digit2, contrast1, contrast2, n_images)
-    labels = torch.tensor([1 if i < n_images // 2 else 0 for i in range(n_images)], dtype=torch.float32)
-    dataset = TensorDataset(blended_images, labels)
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 from copy import deepcopy
 import argparse
@@ -39,216 +13,200 @@ import time
 # Prevent python from saving out .pyc files
 sys.dont_write_bytecode = True
 
-
-batch_size = 32
-digits = (2, 7)  # Pair of digits for blending
-contrast = (0.1, 0.9)  # Contrast levels for each digit
-proportion = (0.6, 0.4)  # Proportions for blending
-n_images = 10000
-
 from architecture import *
 from util import log
-
-#create the datasets
-from shading import create_blended_dataset
+from shading import create_blended_digit_dataset, load_mnist_data  # Importing functions from shading.py
 
 
 def check_path(path):
 	if not os.path.exists(path):
 		os.mkdir(path)
 
-def train(args, encoder, class_out, conf_out, device, train_loader, optimizer, epoch):
-	# Create file for saving training progress
-	train_prog_dir = './train_prog/'
-	check_path(train_prog_dir)
-	model_dir = train_prog_dir + 'run' + str(args.run) + '/'
-	check_path(model_dir)
-	train_prog_fname = model_dir + 'epoch_' + str(epoch) + '.txt'
-	train_prog_f = open(train_prog_fname, 'w')
-	train_prog_f.write('batch class_loss class_acc conf_loss conf\n')
-	# Set to training mode
-	encoder.train()
-	class_out.train()
-	conf_out.train()
-	# Iterate over batches
-	for batch_idx, (data, target) in enumerate(train_loader):
-		# Batch start time
-		start_time = time.time()
-		# Load data
-		x = data.to(device)
-		y_targ = target.to(device)
-		# Scale signal
-		signal = ((torch.rand(x.shape[0]) * (args.		x = x * signal.view(-1, 1, 1, 1)
-		# Scale to [-1, 1]
-		x = (x - 0.5) / 0.5
-		# Threshold image
-		x = nn.Hardtanh()(x)
-		# Zero out gradients for optimizer 
-		optimizer.zero_grad()
-		# Get model predictions
-		z = encoder(x, device)
-		y_pred = class_out(z).squeeze()
-		conf = conf_out(z).squeeze()
-		# Classification loss
-		class_loss_fn = torch.nn.BCELoss()
-		class_loss = class_loss_fn(y_pred, y_targ.float())
-		# Classification accuracy
-		correct_preds = torch.eq(y_pred.round(), y_targ.float()).float()
-		class_acc = correct_preds.mean().item() * 100.0
-		# Confidence loss
-		conf_loss_fn = torch.nn.BCELoss()
-		conf_loss = conf_loss_fn(conf, correct_preds)
-		# Combine losses and update model
-		combined_loss = class_loss + conf_loss
-		combined_loss.backward()
-		optimizer.step()
-		# Overall confidence 
-		avg_conf = conf.mean().item() * 100.0
-		# Batch duration
-		end_time = time.time()
-		batch_dur = end_time - start_time
-		# Report prgoress
-		if batch_idx % args.log_interval == 0:
-			log.info('[Epoch: ' + str(epoch) + '] ' + \
-					 '[Batch: ' + str(batch_idx) + ' of ' + str(len(train_loader)) + '] ' + \
-					 '[Class. Loss = ' + '{:.4f}'.format(class_loss.item()) + '] ' + \
-					 '[Class. Acc. = ' + '{:.2f}'.format(class_acc) + '] ' + \
-					 '[Conf. Loss = ' + '{:.4f}'.format(conf_loss.item()) + '] ' + \
-					 '[Conf. = ' + '{:.2f}'.format(avg_conf) + '] ' + \
-					 '[' + '{:.3f}'.format(batch_dur) + ' sec/batch]')
-			# Save progress to file
-			train_prog_f.write(str(batch_idx) + ' ' +\
-				               '{:.4f}'.format(class_loss.item()) + ' ' + \
-				               '{:.2f}'.format(class_acc) + ' ' + \
-				               '{:.4f}'.format(conf_loss.item()) + ' ' + \
-				               '{:.2f}'.format(avg_conf) + '\n')
-	train_prog_f.close()
+def train(args, encoder, class_out, conf_out, device, train_loader, optimizer, criterion, epoch):
+    train_prog_dir = './train_prog/'
+    check_path(train_prog_dir)
+    model_dir = train_prog_dir + 'run' + str(epoch) + '/'
+    check_path(model_dir)
+    train_prog_fname = model_dir + 'epoch_' + str(epoch) + '.txt'
+    
+    with open(train_prog_fname, 'w') as train_prog_f:
+        train_prog_f.write('batch class_loss class_acc conf_correct conf_incorrect\\n')
+        
+        # Debug: Check types before training
+        print("Debug: Type of encoder:", type(encoder))
+        print("Debug: Type of class_out:", type(class_out))
+        print("Debug: Type of conf_out:", type(conf_out))
 
-def s1s2_test(args, encoder, class_out, conf_out, device, s1_loader, s2_loader, targ_	# Set to evaluation mode
-	encoder.eval()
-	class_out.eval()
-	conf_out.eval()
-	# Iterate over batches
-	all_test_correct_preds = []
-	all_test_conf = []
-	for batch_idx, ((data_s1, target_s1), (data_s2, target_s2)) in enumerate(zip(s1_loader, s2_loader)):
-		# Load data
-		x_s1 = data_s1.to(device)
-		x_s2 = data_s2.to(device)
-		# Sample targets and set signal level for s1/s2
-		y_targ = torch.rand(args.test_batch_size).round().to(device)
-		targ_signal = torch.ones(args.test_batch_size).to(device) * targ_signal
-		nontarg_signal = torch.ones(args.test_batch_size).to(device) * nontarg_signal
-		s1_signal = (targ_signal * torch.logical_not(y_targ).float()) + (nontarg_signal * y_targ)
-		s2_signal = (targ_signal * y_targ) + (nontarg_signal * torch.logical_not(y_targ).float())
-		# Apply contrast scaling and sumperimpose images
-		x_s1 = x_s1 * s1_signal.view(-1,1,1,1)
-		x_s2 = x_s2 * s2_signal.view(-1,1,1,1)
-		x, _ = torch.stack([x_s1, x_s2],0).max(0)
-		# Scale to [-1, 1]
-		x = (x - 0.5) / 0.5
-		# Add noise
-		x = x + (torch.randn(x.shape) * noise).to(device)
-		# Threshold image
-		x = nn.Hardtanh()(x)
-		# Get model predictions
-		z = encoder(x, device)
-		y_pred = class_out(z).squeeze()
-		conf = conf_out(z).squeeze()
-		# Collect responses
-		# Correct predictions
-		correct_preds = torch.eq(y_pred.round(), y_targ.float()).float()
-		all_test_correct_preds.append(correct_preds.detach().cpu().numpy())
-		# Confidence
-		all_test_conf.append(conf.detach().cpu().numpy())
-	# Average test accuracy and confidence
-	all_test_correct_preds = np.concatenate(all_test_correct_preds)
-	all_test_conf = np.concatenate(all_test_conf)
-	avg_test_acc = np.mean(all_test_correct_preds) * 100.0
-	avg_test_conf = np.mean(all_test_conf) * 100.0
-	avg_test_conf_correct = np.mean(all_test_conf[all_test_correct_preds==1]) * 100.0
-	avg_test_conf_incorrect = np.mean(all_test_conf[all_test_correct_preds==0]) * 100.0
-	return avg_test_acc, avg_test_conf, avg_test_conf_correct, avg_test_conf_incorrect
+        # Set models to training mode
+        encoder.train()
+        class_out.train()
+        conf_out.train()
+        
+        # Iterate over batches
+        for batch_idx, (data, target) in enumerate(train_loader):
+            start_time = time.time()
+            data, target = data.to(device), target.to(device)
+            
+            # Zero out gradients for optimizer
+            optimizer.zero_grad()
+            
+            # Forward pass through the encoder and classification/confidence heads
+            z = encoder(data, device)
+            y_pred = class_out(z)
+            conf = conf_out(z)
+            
+            # Calculate losses
+            class_loss = criterion(y_pred, target.float().view(-1, 1))  # Ensure target is correct shape
+            correct_preds = (y_pred.round() == target.float().view(-1, 1)).float()
+            class_acc = correct_preds.mean().item() * 100
+            conf_loss = criterion(conf, correct_preds)  # Using same loss function for simplicity
+            
+            # Combine losses and update model parameters
+            combined_loss = class_loss + conf_loss
+            combined_loss.backward()
+            optimizer.step()
+            
+            # Calculate confidence for correct and incorrect classifications
+            conf_correct = conf[correct_preds.bool()].mean().item() * 100
+            conf_incorrect = conf[~correct_preds.bool()].mean().item() * 100 if (~correct_preds.bool()).any() else 0
+            
+            end_time = time.time()
+            batch_duration = end_time - start_time
+            
+            if batch_idx % 10 == 0:
+                log_info = f'[Epoch: {epoch}] [Batch: {batch_idx}/{len(train_loader)}] ' + \
+                           f'[Class Loss: {class_loss.item():.4f}] [Class Acc: {class_acc:.2f}%] ' + \
+                           f'[Conf Correct: {conf_correct:.2f}%] [Conf Incorrect: {conf_incorrect:.2f}%] ' + \
+                           f'[{batch_duration:.3f} sec/batch]'
+                log.info(log_info)
+                train_prog_f.write(f'{batch_idx} {class_loss.item():.4f} {class_acc:.2f} {conf_correct:.2f} {conf_incorrect:.2f}\n')
+
+
+
+def s1s2_test(args, encoder, class_out, conf_out, device, s1_loader, s2_loader):
+    # Set to evaluation mode
+    encoder.eval()
+    class_out.eval()
+    conf_out.eval()
+
+    # Iterate over batches
+    all_test_correct_preds = []
+    all_test_conf_correct = []
+    all_test_conf_incorrect = []
+    
+    for batch_idx, ((data_s1, target_s1), (data_s2, target_s2)) in enumerate(zip(s1_loader, s2_loader)):
+        # Load data
+        x_s1 = data_s1.to(device)
+        x_s2 = data_s2.to(device)
+        
+        # Get model predictions for both datasets
+        z_s1 = encoder(x_s1, device)
+        y_pred_s1 = class_out(z_s1).squeeze()
+        conf_s1 = conf_out(z_s1).squeeze()
+
+        z_s2 = encoder(x_s2, device)
+        y_pred_s2 = class_out(z_s2).squeeze()
+        conf_s2 = conf_out(z_s2).squeeze()
+
+        # Calculate metrics for s1
+        correct_preds_s1 = (y_pred_s1.round() == target_s1.float().to(device)).float()
+        all_test_correct_preds.append(correct_preds_s1.detach().cpu().numpy())
+        all_test_conf_correct.extend(conf_s1[correct_preds_s1.bool()].detach().cpu().numpy())
+        all_test_conf_incorrect.extend(conf_s1[~correct_preds_s1.bool()].detach().cpu().numpy())
+
+        # Calculate metrics for s2
+        correct_preds_s2 = (y_pred_s2.round() == target_s2.float().to(device)).float()
+        all_test_correct_preds.append(correct_preds_s2.detach().cpu().numpy())
+        all_test_conf_correct.extend(conf_s2[correct_preds_s2.bool()].detach().cpu().numpy())
+        all_test_conf_incorrect.extend(conf_s2[~correct_preds_s2.bool()].detach().cpu().numpy())
+
+    # Aggregate all results
+    all_test_correct_preds = np.concatenate(all_test_correct_preds)
+    avg_test_acc = np.mean(all_test_correct_preds) * 100.0
+    avg_test_conf_correct = np.mean(all_test_conf_correct) * 100.0 if all_test_conf_correct else 0
+    avg_test_conf_incorrect = np.mean(all_test_conf_incorrect) * 100.0 if all_test_conf_incorrect else 0
+
+    return avg_test_acc, avg_test_conf_correct, avg_test_conf_incorrect
 
 def main():
-
-	# Settings
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--train-batch-size', type=int, default=32)
-	parser.add_argument('--test-batch-size', type=int, default=100)
-	parser.add_argument('--	parser.add_argument('--	parser.add_argument('--	parser.add_argument('--noise_level', type=float, default=0.9, help='Noise level for training labels')
-	parser.add_argument('--img_size', type=int, default=32)
-	parser.add_argument('--latent_dim', type=int, default=100)
-	parser.add_argument('--epochs', type=int, default=5)
-	parser.add_argument('--lr', type=float, default=5e-4)
-	parser.add_argument('--no-cuda', action='store_true', default=False)
-	parser.add_argument('--log_interval', type=int, default=10)
-	parser.add_argument('--device', type=int, default=0)
-	parser.add_argument('--run', type=str, default='1')
-	parser.add_argument('--	parser.add_argument('--	args = parser.parse_args()
-		
-	# Set up cuda	
-	use_cuda = not args.no_cuda and torch.cuda.is_available()
-	device = torch.device("cuda:" + str(args.device) if use_cuda else "cpu")
-	kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-
-	# Training set loader
-	train_loader = generate_data_loader(digit1, digit2, contrast1, contrast2, n_images, batch_size)
-	test_loader = generate_data_loader(digit1, digit2, contrast1, contrast2, n_images // 10, batch_size)
+    # Settings
+    parser = argparse.ArgumentParser(description='Train and evaluate the model on a blended MNIST dataset')
+    parser.add_argument('--digit1', type=int, default=1, help='First digit to blend')
+    parser.add_argument('--digit2', type=int, default=0, help='Second digit to blend')
+    parser.add_argument('--n_images', type=int, default=1000, help='Number of images to blend for each digit')
+    parser.add_argument('--contrast1', type=float, default=0.1, help='Contrast adjustment for the first digit')
+    parser.add_argument('--contrast2', type=float, default=0.9, help='Contrast adjustment for the second digit')
+    parser.add_argument('--train-batch-size', type=int, default=64, help='Batch size for training')
+    parser.add_argument('--test-batch-size', type=int, default=1000, help='Batch size for testing')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train')
+    parser.add_argument('--lr', type=float, default=0.01, help='Learning rate for the optimizer')
+    parser.add_argument('--momentum', type=float, default=0.5, help='Momentum for the optimizer')
+    parser.add_argument('--latent_dim', type=int, default=100, help='Dimensionality of the latent space')
+    parser.add_argument('--no_cuda', action='store_true', help='Disables CUDA training')
+    parser.add_argument('--run', type=int, default=1, help='Run identifier')
+    args = parser.parse_args()
 
 
-	# Build model
-	log.info('Building model...')
-	encoder = Encoder(args).to(device)
-	class_out = Class_out(args).to(device)
-	conf_out = Conf_out(args).to(device)
-	all_modules = nn.ModuleList([encoder, class_out, conf_out])
+    # Set up cuda
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device(f"cuda:{args.device}" if use_cuda else "cpu")
+    # Determine CUDA usage based on the no_cuda flag and CUDA availability
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    # Load MNIST data and create blended dataset
+    mnist_data = load_mnist_data()
+    blended_data = create_blended_digit_dataset(mnist_data, args.digit1, args.digit2, args.n_images, args.contrast1, args.contrast2)
+
+    # Generate labels for the blended dataset
+    # Assuming labels should be 0 for digit1 and 1 for digit2, or vice versa
+    labels = torch.zeros(blended_data.size(0), dtype=torch.long)
+    labels[args.n_images:] = 1  # Assuming first half is digit1, second half is digit2
+
+    # Splitting blended dataset into train and test sets
+    # This is a simple split; adjust the ratio as necessary
+    split_index = int(0.8 * len(blended_data))
+    train_data, test_data = blended_data[:split_index], blended_data[split_index:]
+    train_labels, test_labels = labels[:split_index], labels[split_index:]
+
+    # Create TensorDatasets and DataLoaders
+    train_dataset = TensorDataset(train_data, train_labels)
+    test_dataset = TensorDataset(test_data, test_labels)
+    train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False)
+
+    # Device setup
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Model setup (assuming Net() and other models are correctly defined elsewhere)
+# Build model
+    log.info('Building model...')
+    encoder = Encoder(args).to(device)
+    class_out = Class_out(args).to(device)
+    conf_out = Conf_out(args).to(device)
+    all_modules = nn.ModuleList([encoder, class_out, conf_out])
 
 	# Create optimizer
-	log.info('Setting up optimizer...')
-	optimizer = optim.Adam(all_modules.parameters(), lr=args.lr)
+    log.info('Setting up optimizer...')
+    optimizer = torch.optim.Adam([{'params': encoder.parameters()}, {'params': class_out.parameters()}, {'params': conf_out.parameters()}], lr=0.001)
 
-	# Train
-	log.info('Training begins...')
-	for epoch in range(1, args.epochs + 1):
-		# Training loop
-		train(args, encoder, class_out, conf_out, device, train_loader, optimizer, epoch)
+    # Define the loss criterion
+    criterion = torch.nn.CrossEntropyLoss()  # Or another appropriate loss function
 
-	# Test
-	log.info('Test...')
-	# Evaluate without noise
-	test_acc_noiseless, _, __, ___ = test(args, encoder, class_out, conf_out, device, test_loader, 	# Signal and noise values for test
-	signal_test_vals = np.linspace(args.	noise_test_vals = np.linspace(args.	all_test_acc = []
-	all_test_conf = []
-	all_test_conf_correct = []
-	all_test_conf_incorrect = []
-	for n in range(noise_test_vals.shape[0]):
-		all_signal_test_acc = []
-		all_signal_test_conf = []
-		all_signal_test_conf_correct = []
-		all_signal_test_conf_incorrect = []
-		for s in range(signal_test_vals.shape[0]):
-			test_acc, test_conf, test_conf_correct, test_conf_incorrect = test(args, encoder, class_out, conf_out, device, test_loader, 			all_signal_test_acc.append(test_acc)
-			all_signal_test_conf.append(test_conf)
-			all_signal_test_conf_correct.append(test_conf_correct)
-			all_signal_test_conf_incorrect.append(test_conf_incorrect)
-		all_test_acc.append(all_signal_test_acc)
-		all_test_conf.append(all_signal_test_conf)
-		all_test_conf_correct.append(all_signal_test_conf_correct)
-		all_test_conf_incorrect.append(all_signal_test_conf_incorrect)
-	# Save test results
-	test_dir = './test/'
-	check_path(test_dir)
-	model_dir = test_dir + 'run' + str(args.run) + '/'
-	check_path(model_dir)
-	np.savez(model_dir + 'test_results.npz',
-			 signal_test_vals=signal_test_vals,
-			 noise_test_vals=noise_test_vals,
-			 test_acc_noiseless=test_acc_noiseless,
-			 all_test_acc=np.array(all_test_acc),
-			 all_test_conf=np.array(all_test_conf),
-			 all_test_conf_correct=np.array(all_test_conf_correct),
-			 all_test_conf_incorrect=np.array(all_test_conf_incorrect))
+    # Training loop
+    for epoch in range(1, args.epochs + 1):
+        train(args, encoder, class_out, conf_out, device, train_loader, optimizer, criterion, epoch)
+
+
+    # Save test results
+    test_dir = './test/'
+    check_path(test_dir)
+    model_dir = f"{test_dir}run{args.run}/"
+    check_path(model_dir)
+    test_acc, test_conf,test_conf_correct, test_conf_incorrect = s1s2_test(encoder, device, test_loader, criterion)
+
+    np.savez(model_dir + 'test_results.npz', test_acc=test_acc, test_conf=test_conf,
+             test_conf_correct=test_conf_correct, test_conf_incorrect=test_conf_incorrect)
+
 
 if __name__ == '__main__':
 	main()
